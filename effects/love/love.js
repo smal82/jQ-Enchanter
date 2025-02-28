5f9e35d4389057c0e18e6eac3384e41942df74be
smal <47319874+smal82@users.noreply.github.com>
2025-02-28 10:18:12 +0100

    Add files via upload

---
+++ effects/love/love.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+	
+	$('#loveText').css('opacity', 1); // Avvia l'animazione del testo});
+	
+	});
---
+++ effects/love/love.php
@@ -0,0 +1,13 @@
+<?php
+// Silenzia gli errori se il file è incluso direttamente
+if (!defined('ABSPATH')) exit;
+?>
+    <div id="fixedText">
+    <div id="loveText">
+      <span class="letter">L</span>
+      <span class="letter">O</span>
+      <span class="letter">V</span>
+      <span class="letter">E</span>
+      <span class="heart">❤️</span>
+    </div>
+  </div>
